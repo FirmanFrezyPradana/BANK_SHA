0ee209f873e4b634d78d440b72c46e10b93298ee
“FirmanFrezyPradana” <dana.firman16@gmail.com>
2024-09-28 08:59:39 +0700

    2.slicing splash & onboarding page

---
+++ lib/Ui/Pages/onboarding_page.dart
@@ -0,0 +1,213 @@
+import 'package:bank_sha/Shared/theme.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class onboardingPage extends StatefulWidget {
+  const onboardingPage({super.key});
+
+  @override
+  State<onboardingPage> createState() => _onboardingPageState();
+}
+
+class _onboardingPageState extends State<onboardingPage> {
+  int currentIndex = 1;
+  CarouselSliderController carouselController = CarouselSliderController();
+
+  List<String> titles = [
+    'Grow Your\nFinancial Today',
+    'Build From\nZero to Freedom',
+    'Start Together'
+  ];
+  List<String> subtitles = [
+    'Our system is helping you to\nachieve a better goal',
+    'We provide tips for you so that\nyou can adapt easier',
+    'We will guide you to where\nyou wanted it too'
+  ];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  width: 250,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  width: 250,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  width: 300,
+                ),
+              ],
+              options: CarouselOptions(
+                height: 531,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(
+                    () {
+                      currentIndex = index;
+                    },
+                  );
+                },
+              ),
+              carouselController: carouselController,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 22, vertical: 24),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    titles[currentIndex],
+                    style: blackTetxtStyle.copyWith(
+                        fontSize: 20, fontWeight: semiBold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    subtitles[currentIndex],
+                    style: grayTetxtStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: currentIndex == 2 ? 38 : 40,
+                  ),
+                  currentIndex == 2
+                      ? Column(
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: TextButton(
+                                onPressed: () {
+                                  carouselController.nextPage();
+                                },
+                                style: TextButton.styleFrom(
+                                  backgroundColor: purpleColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                    56,
+                                  )),
+                                ),
+                                child: Text(
+                                  'Get Started',
+                                  style: whiteTetxtStyle.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Sign In',
+                                  style: grayTetxtStyle.copyWith(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      : Row(
+                          children: [
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: currentIndex == 0
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: currentIndex == 1
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: currentIndex == 2
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            const Spacer(),
+                            SizedBox(
+                              width: 150,
+                              height: 50,
+                              child: TextButton(
+                                onPressed: () {
+                                  carouselController.nextPage();
+                                },
+                                style: TextButton.styleFrom(
+                                  backgroundColor: purpleColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                    56,
+                                  )),
+                                ),
+                                child: Text(
+                                  'Continue',
+                                  style: whiteTetxtStyle.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: bold,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Ui/Pages/splash_page.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bank_sha/Shared/theme.dart';
+import 'package:bank_sha/Ui/Pages/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const onboardingPage()));
+    });
+  }
+  // context,MaterialPageRoute(Builder:(context)=>onBoardingPage())
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/img_logo_dark.png'),
+          )),
+        ),
+      ),
+    );
+  }
+}
